726561ecb5022bb9bdc18f68f260af3b6e310ff6
victorShiva <141312814+victorShiva@users.noreply.github.com>
2024-05-02 11:27:14 +0000

    variable

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=5775;
+let accountEmail='shivagupta@gmail.com';
+var accountPassword='8858585';
+accountCity='jaunpur';
+let accountState;
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
