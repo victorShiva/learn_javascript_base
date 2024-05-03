43f8e4b91785584ac5ae44ec3cc77008466d9ce8
victorShiva <141312814+victorShiva@users.noreply.github.com>
2024-05-03 11:20:45 +0000

    learn datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as new version
+
+// alert('Hello world!') //we are using nodejs not browser
+
+let name="shiva";
+let age=25;
+let isLoggedIn=true;   
+let state; 
+
+// ----------------primitive datatypes
+
+//number => 2 to power 53
+//bigint
+//string
+//boolean => true/false
+//null   => standalone value
+//undefined
+//symbol => unique
+
+//----------------non primitive datatypes
+ 
+//object
+
+console.log(typeof "shiva");
+console.log(typeof age);
+
+console.log(typeof null);
+console.log(typeof undefined);
