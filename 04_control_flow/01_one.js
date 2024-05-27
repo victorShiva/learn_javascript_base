cc08ef9234d2ccc655e804d235b08f7756dbad92
victorShiva <141312814+victorShiva@users.noreply.github.com>
2024-05-27 11:17:35 +0000

    learn truethy value

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,33 @@
+const isUserLoggedIn = true;
+if(isUserLoggedIn){
+    console.log("User is Login");
+}
+
+
+
+// if(2 == "2"){
+//     console.log("Executed");
+// }
+
+
+
+// const score = 200;
+// if(score > 100){
+//     let power = "fly";
+//     console.log(`User Power : ${power}`);
+// }
+// console.log(`User Power : ${power}`);  // Error power is not defined
+
+
+
+
+// const balance = 200;
+// if(balance < 500) console.log("One") , console.log("Two");
+
+
+const loggedInFormGoogle = true;
+const loggedInFormEmail=false;
+
+if(loggedInFormGoogle ||loggedInFormEmail ){
+    console.log("User can Logged In");
+}
