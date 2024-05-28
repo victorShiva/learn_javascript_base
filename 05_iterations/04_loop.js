5014d745c2ea70810a851ee3d1e251d5f0356bb4
Shiv Shankar Agrahari <141312814+victorShiva@users.noreply.github.com>
2024-05-28 08:18:40 +0000

    learn for in loop

---
+++ 05_iterations/04_loop.js
@@ -0,0 +1,42 @@
+// for in loop
+
+
+const newObj = {
+    js: "javascript",
+    cpp: "c++",
+    rb: "ruby",
+    swift: "swift",
+    prop5: "value5",
+    prop6: "value6"
+};
+
+// for (const key in newObj) {
+//     // console.log(newObj[key]);
+//     console.log(`${key} values is ${newObj[key]}`);
+// }
+
+
+
+const programmingLanguages = ["JavaScript", "Python", "Java", "C++", "C#", "Ruby"];
+// for (const key in programmingLanguages) {
+//     console.log(key);
+// }
+
+// for (const key in programmingLanguages) {
+//     console.log(programmingLanguages[key]);
+// }
+
+
+
+
+// const map = new Map();
+
+// map.set("IN", "India");
+// map.set("USA", "United States Of America");
+// map.set("FR", "France");
+// map.set("IN", "India");
+
+
+// for (const key in map) {
+//     console.log(key);
+// }
