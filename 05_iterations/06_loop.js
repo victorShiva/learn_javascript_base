de2cf31bbb0faa0f1fae3ea27646d825facc57aa
Shiv Shankar Agrahari <141312814+victorShiva@users.noreply.github.com>
2024-05-28 09:01:50 +0000

    filter methods

---
+++ 05_iterations/06_loop.js
@@ -0,0 +1,27 @@
+// const coding = ["JavaScript", "Python", "Java", "C++", "C#", "Ruby"];
+
+// const values = coding.forEach((item)=>{
+//     return item;
+// })
+// console.log(values);                //undefined
+
+
+
+
+
+
+
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+
+// let newNums = myNums.filter((num)=>num>4);
+// console.log(newNums);                    // [ 6, 7, 8, 9, 10 ]
+
+let newNums = myNums.filter((num)=>{
+    return num>5;
+})
+// console.log(newNums);                   //[ 6, 7, 8, 9, 10 ]
+
+
+// using forEach 
+
+
