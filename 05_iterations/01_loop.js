a59e4f548d4fbde8970be180a1b48a60767a70fc
Shiv Shankar Agrahari <141312814+victorShiva@users.noreply.github.com>
2024-05-28 06:48:37 +0000

    for loop learn

---
+++ 05_iterations/01_loop.js
@@ -0,0 +1,42 @@
+// for (let num = 0; num < 10; num++) {
+//    console.log(num);
+//    if(num == 5){
+//     console.log('numb 5 is best number');
+//    }
+// }
+
+
+
+// for (let i = 0; i < 5; i++) {
+//     console.log('outer is ',i);
+//     for (let j = 10; j < 15; j++) {
+//        console.log(j);
+//     }
+// }
+
+
+const superHeroes = ["Superman", "Batman", "Wonder Woman", "Spiderman", "Iron Man", "Hulk", "Flash"];
+for (let i = 0; i < superHeroes.length; i++) {
+    // console.log(superHeroes[i]);    
+}
+
+
+
+// break and countinue
+for (let i = 1; i < 20; i++) {
+    if(i == 5) {
+        console.log('5 detected');
+        continue;
+    }
+    console.log(`value of i is ${i}`);
+}
+
+
+
+// for (let i = 1; i < 20; i++) {
+//     if(i == 5) {
+//         console.log('5 detected');
+//         break;
+//     }
+//     console.log(`value of i is ${i}`);
+// }
