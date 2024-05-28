cba4e3e3bbc49498a5238a9dc232cd94561fb3c3
victorShiva <141312814+victorShiva@users.noreply.github.com>
2024-05-28 04:05:19 +0000

    learn nullish and ternary operator

--- 04_control_flow/02_truethy.js
+++ 04_control_flow/02_truethy.js
@@ -1,9 +1,9 @@
 const userEmail= "shiva@.ai";
-if(userEmail){
-    console.log("Got User Email");
-}else{
-    console.log("Don't have user Email");
-}
+// if(userEmail){
+//     console.log("Got User Email");
+// }else{
+//     console.log("Don't have user Email");
+// }
 
 
 //falsy values :->          false , 0 , -0 , BigInt 0n , "" , null , undefined , NaN
@@ -17,12 +17,46 @@ let fruits = [];
 // }
 
 
-if(fruits.length === 0){
-    console.log("Array is Empty");
-}
+// if(fruits.length === 0){
+//     console.log("Array is Empty");
+// }
 
 
-let emptyObject = {};
-if(Object.keys(emptyObject).length === 0 ){
-    console.log("emptyObject is Empty");
-}
+// let emptyObject = {};
+// if(Object.keys(emptyObject).length === 0 ){
+//     console.log("emptyObject is Empty");
+// }
+
+
+
+
+// Nullish Coalescing Operator (??) : null undefined
+
+let val1;
+val1 = 5 ?? 10;              //5
+val1 = null ?? 10;           // 10
+val1 = undefined ?? 15;     //15
+val1 = null ?? 12 ?? 34;     // 12
+
+console.log(val1);
+
+
+
+// Ternary Operator
+// condition ? true : false ;
+
+const icePrice = 80;
+icePrice >= 100 ? console.log('more than 100') : console.log('less than 100');
+
+
+// function greet(isMorning) {
+//     return isMorning ? 'Good morning!' : 'Good evening!';
+// }
+// console.log(greet(true));  // Output: Good morning!
+// console.log(greet(false)); // Output: Good evening!
+
+
+let age = 18;
+let canVote = age >= 18 ? 'Yes, you can vote.' : 'No, you cannot vote.' ;
+
+console.log(canVote);
