d3714d58df0722c30e61c8dec9b9bfce0e5f2040
Shiv Shankar Agrahari <141312814+victorShiva@users.noreply.github.com>
2024-05-28 07:59:53 +0000

    map object learn

---
+++ 05_iterations/03_loop.js
@@ -0,0 +1,36 @@
+// const superHeroes = ["Superman", "Batman", "Wonder Woman", "Spiderman", "Iron Man", "Hulk", "Flash"];
+// for (const hero of superHeroes) {
+//     console.log(hero);    
+// }
+
+// const str = "my name is khan";
+// for(const char of str){
+//     console.log(char);
+// }
+
+
+//Maps
+const map = new Map();
+
+map.set("IN", "India");
+map.set("USA", "United States Of America");
+map.set("FR", "France");
+map.set("IN", "India");
+
+// console.log(map);                           //Map(3) {'IN' => 'India','USA' => 'United States Of America','FR' => 'France'} 
+
+
+// for (const [key,value] of map) {
+//     console.log(key);                           //IN USA FR
+// }
+
+
+// Object
+const myObj = {
+    'game1': 'NF5',
+    game2 : 'spiderMan',
+    game3 : 'PUBG'
+}
+// for (const [key,value] of myObj) {
+//     console.log(key);                        // Error Object is not iteratable
+// }
