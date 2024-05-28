d83dc2fc3473e153ec7ba90a30d357ada5f82ec1
Shiv Shankar Agrahari <141312814+victorShiva@users.noreply.github.com>
2024-05-28 07:00:38 +0000

    learn while and dowhile loop

---
+++ 05_iterations/02_loop.js
@@ -0,0 +1,21 @@
+
+// let i =0;
+// while (i<=10) {
+//     console.log(`value of i ${i}`);
+//     i = i+2;
+// }
+
+const superHeroes = ["Superman", "Batman", "Wonder Woman", "Spiderman", "Iron Man", "Hulk", "Flash"];
+
+// let i =0;
+// while (i < superHeroes.length) {
+//     console.log(superHeroes[i]);
+//     i++;
+// }
+
+
+let score = 1;
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score <=10);
